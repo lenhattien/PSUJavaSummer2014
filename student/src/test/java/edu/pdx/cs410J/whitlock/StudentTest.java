cf20ed7e9ccfefcac1aae1c8b0d6d09491bc4b2e
Tien Le <letien@pdx.edu>
2014-07-16 16:33:26 -0700

    all students say this class is too much work test case

--- student/src/test/java/edu/pdx/cs410J/whitlock/StudentTest.java
+++ student/src/test/java/edu/pdx/cs410J/whitlock/StudentTest.java
@@ -195,6 +195,12 @@ public class StudentTest extends InvokeMainTestCase
     assertThat(student.toString(), containsString("He says"));
   }
 
+  @Test
+  public void allStudentsSayThisClassIsTooMuchWorkToString() {
+    Student student = new Student("Name", MALE, 3.45, new ArrayList<String>());
+    assertThat(student.toString(), containsString("\"This class is too much work\"."));
+  }
+
   @Ignore
   @Test
   public void commandLineExampleFromAssignment() {
